ca635177df11963c198710457aa739bd5e8281da
gewenrui <941137860@qq.com>
2017-05-22 09:05:09 +0800

    first commint

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,81 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    Fuck Vue
+
+    <!--  v-html 渲染-->
+   <div v-html="message">
+    </div>
+
+
+    <button v-on:click="test">开始</button>
+
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'hello',
+    created(){
+
+    },
+    data () {
+      return {
+        //文本中实际输出内容
+        message: '',
+        content: `
+          今天天气很好吗,
+          今天天气很好吗,
+          今天天气很好吗,
+          今天天气很好吗,
+          今天天气很好吗,
+          今天天气很好吗,
+          今天天气很好吗,
+          今天天气很好吗,
+          今天天气很好吗
+          `,
+        //计数器
+        counter: 0,
+        //数组总长度
+        totalLength: 0,
+        //分词后的数组
+        splitArray: {},
+        //setIntval的id
+        id: 0
+
+      }
+    }, methods: {
+      test: function () {
+        //数组
+        this.$data.splitArray = this.$data.content.split(",");
+        //数组长度
+        this.$data.totalLength = this.$data.splitArray.length;
+        //每个setInterval都会有一个Id
+        this.$data.id = setInterval(this.loop, 1000)
+      }, loop: function () {
+        //如果当前元素大于数组的总元素的话,那么直接提示数组过长
+        if (this.$data.counter >= this.$data.totalLength) {
+          //清除setInterval
+          clearInterval(this.$data.id);
+          //return null
+          return;
+        }
+        //console.log(this.$data.splitArray);
+
+        //清空左右空格,需要check数组内元素是否为空
+        this.$data.message += this.$data.splitArray[this.$data.counter] != "" ? this.$data.splitArray[this.$data.counter].trim() : '';
+        this.$data.message += "<br>"
+
+        //计数器加一
+        this.$data.counter += 1;
+
+
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
