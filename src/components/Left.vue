85ba919faf916b9d27714f85a447b75b44835ca7
gewenrui <941137860@qq.com>
2017-05-22 17:15:44 +0800

    update code

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -4,6 +4,8 @@
     <div class="div-inline" id="title">
       GitHub
 
+
+
     </div>
     <div class="div-inline" id="left">
       <!--  v-html 渲染-->
@@ -12,7 +14,8 @@
 
       <button v-on:click="test">开始</button>
       <button v-on:click="show">
-        <开始______></开始______>
+        change
+
       </button>
     </div>
 
@@ -21,26 +24,32 @@
 
       <div id="right-content" v-bind:style="right_content">
         <!--第一部分-->
-        <div id="first_part">
+        <div id="first_part" v-bind:style="first_part">
           葛文瑞
           <br>
           哈尔滨理工大学|软件工程系
           <br>
           邮箱:gewenrui0.0@gmail.com
 
+
+
           <hr>
         </div>
         <!--第二部分-->
-        <div id="second_part">
-          <span id="title_one">工作经验:</span>
+        <div id="second_part" v-bind:style="second_part">
+          <span id="title_one" v-bind:style="title_one">工作经验:</span>
 
           <li>
             负责公司CRM项目的搭建,前端采用开源模板AdminLTE,后端使用YAF+FluentPdo进行开发.主要实现了对用户,保单等大量数据的查询.通过定时任务调取微信/支付宝api,实现财务自动退款,编写脚本实现跟单数据来源以及外呼业务。
 
 
+
+
           </li>
           <li>
             负责公司CMS项目(Phalcon)的理赔模块的开发维护,主要实现了与第三方TPA数据对接,涉及到用户端上传理赔信息/图片,并对图片进行压缩,编写定时脚本将数据传输至SFTP服务器,自动发送邮件告知第三方公司,数据回传后,可以通过微信打款,并发送微信模板消息告知用户,前端采用React.js编写,使用webpack打包工具对代码进行打包编译。
+
+
 
 
           </li>
@@ -53,8 +62,8 @@
           <hr>
         </div>
         <!--第三部分-->
-        <div id="third_part">
-          <span id="title_two">个人技能:</span>
+        <div id="third_part" v-bind:style="third_part">
+          <span id="title_two" v-bind:style="title_two">个人技能:</span>
           <li>熟练使用php进行开发,熟悉创建的设计模式</li>
           <li>熟练使用git版本控制工具</li>
           <li>熟练使用Linux</li>
@@ -107,68 +116,97 @@ pre{white-space:pre-wrap; white-space:-moz-pre-wrap; white-space:-pre-wrap; whit
 .article_description { text-indent:2em;}
           `,
         //计数器
-        counter: 0,
+        leftCounter: 0,
         //数组总长度
-        totalLength: 0,
+        leftTotalLength: 0,
         //分词后的数组
-        splitArray: {},
-        //setIntval的id
-        id: 0,
-        activeColor: 'red',
-        fontSize: 30,
-        styleObject: {
-          color: 'red',
-          fontSize: '10px'
-        }, right_content: {}
+        leftSplitArray: {},
+        //左侧setIntval的id
+        leftId: 0,
+
+        //右侧
+        //右侧计数器
+        rightCounter: 0,
+        //右侧未分词的数组
+        rightSplitArray: {},
+        rightId: 0,
+        rightTotalLength: 0,
+
+
+        //////////////////////以下皆为style初始化语句
+        right_content: {},
+        first_part: {},
+        title_one: {},
+        title_two: {},
+        second_part:{},
+        third_part:{}
 
       }
     }, methods: {
       test: function () {
         //数组
-        this.$data.splitArray = this.$data.content.split(",");
+        this.$data.leftSplitArray = this.$data.content.split(",");
         //数组长度
-        this.$data.totalLength = this.$data.splitArray.length;
+        this.$data.leftTotalLength = this.$data.leftSplitArray.length;
         //每个setInterval都会有一个Id
-        this.$data.id = setInterval(this.loop, 500)
+        this.$data.leftId = setInterval(this.loop, 500)
       }, loop: function () {
         //如果当前元素大于数组的总元素的话,那么直接提示数组过长
-        if (this.$data.counter >= this.$data.totalLength) {
+        if (this.$data.leftCounter >= this.$data.leftTotalLength) {
           //清除setInterval
-          clearInterval(this.$data.id);
+          clearInterval(this.$data.leftId);
           //return null
           return;
         }
         //console.log(this.$data.splitArray);
 
         //清空左右空格,需要check数组内元素是否为空
-        this.$data.message += this.$data.splitArray[this.$data.counter] != "" ? "<span>" + this.$data.splitArray[this.$data.counter].trim() + "</span>" : '';
+        this.$data.message += this.$data.leftSplitArray[this.$data.leftCounter] != "" ? "<span>" + this.$data.leftSplitArray[this.$data.leftCounter].trim() + "</span>" : '';
         this.$data.message += "<br>";
 
         //计数器加一
-        this.$data.counter += 1;
+        this.$data.leftCounter += 1;
 
 
       }, show: function () {
 
-        var dataObj = {}; //声明一个数组
-        //声明一个数组
+        var dataObj = []; //声明一个数组
+        //右侧整个布局
+        dataObj[0] = "right_content/margin/20px";
+        dataObj[1] = "first_part/font-size/10px";
+        dataObj[2] = "title_one/font-weight/bold";
+        dataObj[3] = "title_two/font-weight/bold";
+        dataObj[4] = "second_part/font-size/10px";
+        dataObj[5] = "third_part/font-size/10px";
 
-        dataObj["right_content"] = "margin/20px";
+
+        //获取右侧数组长度
+        this.$data.rightTotalLength = dataObj.length;
+        this.$data.rightSplitArray = dataObj;
         //声明数组,可实现
-        this.$set(this.$data.right_content, "margin", "20px")
         //定时更新style数据
-        setInterval(this.appendStyle, 1000)
-        //this.randColor()
+        this.$data.rightId = setInterval(this.appendStyle, 1000)
+
         //随机append数组内的颜色
       }, appendStyle: function () {
 
-        this.$set(this.$data.styleObject, "color", color[randomNum])
+        var i = 0;
+        for (i; i < this.$data.rightTotalLength; i++) {
+          if (i == this.$data.rightCounter) {
+            //拆分js
+            var result = this.$data.rightSplitArray[this.$data.rightCounter].split('/');
+            var obj_temp = "this.$data." + result[0]
+            this.$set(eval(obj_temp), result[1], result[2])
+            this.$data.rightCounter++
+            break;
+          }
+        }
+
       }
       , randColor: function () {
         var color = ["black", "red", "green", "gray"]
         var randomNum = Math.random() * color.length;
         randomNum = parseInt(randomNum, 10)
-        this.$set(this.$data.styleObject, "color", color[randomNum])
       }
     }
   }
@@ -218,25 +256,25 @@ pre{white-space:pre-wrap; white-space:-moz-pre-wrap; white-space:-pre-wrap; whit
   /*}*/
 
   /*第一部分css*/
-  #first_part {
-    font-size: 10px;
-  }
+  /*#first_part {*/
+  /*font-size: 10px;*/
+  /*}*/
 
-  #second_part {
-    font-size: 10px;
-  }
+  /*#second_part {*/
+    /*font-size: 10px;*/
+  /*}*/
 
-  #third_part {
-    font-size: 10px;
-  }
+  /*#third_part {*/
+    /*font-size: 10px;*/
+  /*}*/
 
-  #title_one {
-    font-weight: bold
-  }
+  /*#title_one {*/
+  /*font-weight: bold*/
+  /*}*/
 
-  #title_two {
-    font-weight: bold
-  }
+  /*#title_two {*/
+    /*font-weight: bold*/
+  /*}*/
 
 
 </style>
